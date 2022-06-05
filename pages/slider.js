e266ab0dc6bd014fbdbde389bdf3f6969c3053de
Chris Kavanagh <ckava3@gmail.com>
2022-06-05 15:31:37 -0400

    dragConstraints

--- pages/slider.js
+++ pages/slider.js
@@ -1,22 +1,26 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { useRouter } from "next/router";
 import pics from "../components/images";
 import style from "../styles/Slider.module.css";
 import { useRef, useEffect, useState } from "react";
 
 function Slider() {
-  const [width, setWidth] = useState(0);
+  const router = useRouter();
+  const [width, setWidth] = useState(0); // -4781
   const carousel = useRef();
 
   useEffect(() => {
-    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    //console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    //console.log(width);
   }, []);
   return (
     <div className={style.slider}>
       <motion.div ref={carousel} className={style.carousel}>
         <motion.div
           drag="x"
-          dragConstraints={{ right: 0 }}
+          dragConstraints={{ right: 0, left: -width }}
           className={style.inner}
         >
           {pics.map((pic, index) => {
